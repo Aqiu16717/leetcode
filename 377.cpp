91eaab4c5d8c39b3e1eab610c8b31c65bc45b762
aq1u <aq1u@outlook.com>
2024-04-04 18:06:17 +0800

    update leetcode 377

--- 377.cpp
+++ 377.cpp
@@ -1,91 +1,104 @@
 #include <vector>
 #include <iostream>
+#include <functional>
 
 using namespace std;
 
 int combinationSum4_V0(vector<int>& nums, int target) {
-    // 初始化dp数组，dp[i]表示和为i的组合个数
-    vector<unsigned int> dp(target + 1, 0);
-    dp[0] = 1;  // 初始情况，和为0的组合只有一个，即空集
-
-    // 遍历目标整数的所有可能取值
-    for (int i = 1; i <= target; ++i) {
-        // 遍历数组nums中的每个元素
-        for (int num : nums) {
-            // 如果i >= num，表示当前目标整数i可以由num和之前的组合构成
-            if (i >= num) {
-                // 更新dp[i]，加上dp[i - num]
-                dp[i] += dp[i - num];
-            }
+    function<int(int)> dfs = [&](int sum)->int {
+        if (sum > target) {
+            return 0;
         }
-    }
+        if (sum == target) {
+            return 1;
+        }
+        int ret = 0;
+        for (int num : nums) {
+            ret += dfs(sum + num);
+        }
+        return ret;
+    };
 
-    // dp[target]就是和为target的组合个数
-    return dp[target];
+    int sum = 0;
+    return dfs(sum);
 }
 
 int combinationSum4_V1(vector<int>& nums, int target) {
-    // 初始化二维dp数组，dp[i][j]表示和为j的组合个数
-    vector<vector<unsigned int>> dp(nums.size() + 1, vector<unsigned int>(target + 1, 0));
-    dp[0][0] = 1;  // 初始情况，和为0的组合只有一个，即空集
-
-    // 遍历数组nums中的每个元素
-    for (int i = 1; i <= nums.size(); ++i) {
-        for (int j = 0; j <= target; ++j) {
-            // dp[i][j]等于不选择当前元素的组合个数
-            dp[i][j] = dp[i - 1][j];
-            
-            // 如果j >= nums[i - 1]，表示当前目标整数j可以由nums[i - 1]和之前的组合构成
-            if (j >= nums[i - 1]) {
-                // 更新dp[i][j]，加上dp[i][j - nums[i - 1]]
-                dp[i][j] += dp[i][j - nums[i - 1]];
-            }
+    function<int(int)> dfs = [&](int sum)->int {
+        if (sum > target) {
+            return 0;
         }
-    }
-
-    std::cout << std::endl;
-    for (auto& vec : dp) {
-        for (auto elem : vec) {
-            std::cout << elem << " ";
+        if (sum == target) {
+            return 1;
         }
-        std::cout << std::endl;
-    }
+        int ret = 0;
+        for (int num : nums) {
+            ret += dfs(sum + num);
+        }
+        return ret;
+    };
 
-    // dp[nums.size()][target]就是和为target的组合个数
-    return dp[nums.size()][target];
+    int sum = 0;
+    return dfs(sum);
 }
 
-// wrong
 int combinationSum4_V2(vector<int>& nums, int target) {
-    // 初始化二维dp数组，dp[i][j]表示和为j的组合个数
-    vector<vector<unsigned int>> dp(nums.size() + 1, vector<unsigned int>(target + 1, 0));
-    dp[0][0] = 1;  // 初始情况，和为0的组合只有一个，即空集
-
-    // 遍历数组nums中的每个元素
-    for (int i = 1; i <= nums.size(); ++i) {
-        for (int j = 0; j <= target; ++j) {
-            // dp[i][j]等于不选择当前元素的组合个数
-            dp[i][j] = dp[i - 1][j];
-            
-            // 如果j >= nums[i - 1]，表示当前目标整数j可以由nums[i - 1]和之前的组合构成
-            if (j >= nums[i - 1]) {
-                // 更新dp[i][j]，加上dp[i][j - nums[i - 1]]
-                dp[i][j] += dp[i][j - nums[i - 1]];
-            }
+    function<int(int)> dfs = [&](int sum)->int {
+        if (sum > target) {
+        return 0;
         }
-    }
-
-    std::cout << std::endl;
-    for (auto& vec : dp) {
-        for (auto elem : vec) {
-            std::cout << elem << " ";
+        if (sum == target) {
+            return 1;
         }
-        std::cout << std::endl;
-    }
+        int ret = 0;
+        for (int num : nums) {
+            ret += dfs(sum + num);
+        }
+        return ret;
+    };
 
-    // dp[nums.size()][target]就是和为target的组合个数
-    return dp[nums.size()][target];
+    int sum = 0;
+    return dfs(sum);
 }
+
+int combinationSum4_V3(vector<int>& nums, int target) {
+    function<int(int)> dfs = [&](int sum)->int {
+        if (sum > target) {
+            return 0;
+        }
+        if (sum == target) {
+            return 1;
+        }
+        int ret = 0;
+        for (int num : nums) {
+            ret += dfs(sum + num);
+        }
+        return ret;
+    };
+
+    int sum = 0;
+    return dfs(sum);  
+}
+
+int combinationSum4_V4(vector<int>& nums, int target) {
+    function<int(int)> dfs = [&](int sum)->int {
+        if (sum > target) {
+            return 0;
+        }
+        if (sum == target) {
+            return 1;
+        }
+        int ret = 0;
+        for (int num : nums) {
+            ret += dfs(sum + num);
+        }
+        return ret;
+    };
+
+    int sum = 0;
+    return dfs(sum);
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -94,5 +107,7 @@ int main(int argc, char const *argv[])
     cout << "V0 Test Case 1: " << combinationSum4_V0(nums1, target1) << " (Expected Output: 7)" << endl;
     cout << "V1 Test Case 1: " << combinationSum4_V1(nums1, target1) << " (Expected Output: 7)" << endl;
     cout << "V2 Test Case 1: " << combinationSum4_V2(nums1, target1) << " (Expected Output: 7)" << endl;
+    cout << "V3 Test Case 1: " << combinationSum4_V3(nums1, target1) << " (Expected Output: 7)" << endl;
+    cout << "V4 Test Case 1: " << combinationSum4_V4(nums1, target1) << " (Expected Output: 7)" << endl;
     return 0;
 }
--- README.md
+++ README.md
@@ -23,9 +23,18 @@
 * [63. Unique Paths II](https://leetcode.cn/problems/unique-paths-ii/)
 * [343. Integer Break](https://leetcode.cn/problems/integer-break/)
 * [96. Unique Binary Search Trees](https://leetcode.cn/problems/unique-binary-search-trees/)
+* [377. Combination Sum IV](https://leetcode.cn/problems/combination-sum-iv/)
 
 ## stock
 
 * [121. Best Time to Buy and Sell Stock](https://leetcode.cn/problems/best-time-to-buy-and-sell-stock)
 * [309. Best Time to Buy and Sell Stock with Cooldown](https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-cooldown/)
 * [714. Best Time to Buy and Sell Stock with Transaction Fee](https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/)
+
+## Combination
+
+* [77. Combinations](https://leetcode.cn/problems/combinations/)
+* [39. Combination Sum](https://leetcode.cn/problems/combination-sum/)
+* [40. Combination Sum II](https://leetcode.cn/problems/combination-sum-ii/)
+* [216. Combination Sum III](https://leetcode.cn/problems/combination-sum-iii/)
+* [377. Combination Sum IV](https://leetcode.cn/problems/combination-sum-iv/)
