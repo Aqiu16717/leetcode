7e37373af22e06c6d3d114d6b7c7007ca6491ddd
aq1u <aq1u@outlook.com>
2024-08-30 17:01:08 +0800

    add prefix_sum

---
+++ prefix_sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> nums = {1, 2, 3, 4, 5};
+    std::cout << "nums:" << std::endl;
+    for (int num : nums) {
+        std::cout << num << "\t";
+    }
+    std::cout << std::endl;
+    int n = nums.size();
+    std::vector<int> prefix_sum(n, 0);
+    prefix_sum[0] = nums[0];
+    for (int i = 1; i < n; ++i) {
+        prefix_sum[i] = prefix_sum[i - 1] + nums[i];
+    }
+    std::cout << "prefix_sum:" << std::endl;
+    for (int i : prefix_sum) {
+        std::cout << i << "\t";
+    }
+    std::cout << std::endl;
+    return 0;
+}
