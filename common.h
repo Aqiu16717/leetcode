f89af15fed3c8cae76c165276652304822e4ffad
aq1u <aq1u@outlook.com>
2024-09-09 10:43:58 +0800

    feat: add print2DVec function to common.h

--- common.h
+++ common.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include <iostream>
-#include <vector>
 #include <set>
+#include <vector>
 
-template<typename T>
+template <typename T>
 void printVec(const std::vector<T>& vec) {
     std::cout << "[ ";
     for (auto elem : vec) {
@@ -13,7 +13,18 @@ void printVec(const std::vector<T>& vec) {
     std::cout << "]" << std::endl;
 }
 
-template<typename T>
+template <typename T>
+void print2DVec(const std::vector<std::vector<T>>& vecs) {
+    for (auto vec : vecs) {
+        std::cout << "[ ";
+        for (auto elem : vec) {
+            std::cout << elem << " ";
+        }
+        std::cout << "]" << std::endl;
+    }
+}
+
+template <typename T>
 void printSet(const std::set<T>& s) {
     std::cout << "[ ";
     for (auto elem : s) {
